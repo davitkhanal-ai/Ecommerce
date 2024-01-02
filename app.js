bd7ce7b0a9e15818f07fb5865154259bbec52feb
davitkhanal-ai <khanaldavit@gmail.com>
2024-01-02 20:44:22 +0545

    created a database and category schema added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const reciperRouter = require("./server/routes/recipeRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// for db connection
+
+
+
 // requiring the dotenv
 require("dotenv").config();
 
@@ -16,6 +20,8 @@ app.use(express.static(__dirname + "/public"));
 app.use(expressLayouts);
 app.set("layout", path.join(__dirname, "views/layouts/main.ejs"));
 app.set("view engine", "ejs");
+
+
 
 // Routes
 app.use("/", reciperRouter);
---
+++ server/database/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "DB Connection erro"));
+db.once("open", function () {
+  console.log("connected to DB");
+});
+
+//models
+require("./category");
+
+module.exports = db;
