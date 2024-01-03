fcfee7e6ad25a37c5b6395febd3a33e7b5c88018
davitkhanal-ai <khanaldavit@gmail.com>
2024-01-03 22:15:08 +0545

    unwanted spaces removed

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000;
 
 // for db connection
 
-
-
 // requiring the dotenv
 require("dotenv").config();
 
@@ -20,8 +18,6 @@ app.use(express.static(__dirname + "/public"));
 app.use(expressLayouts);
 app.set("layout", path.join(__dirname, "views/layouts/main.ejs"));
 app.set("view engine", "ejs");
-
-
 
 // Routes
 app.use("/", reciperRouter);
