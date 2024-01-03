daa4ec62f24a1d4e166f5c15b4933409db113d85
davitkhanal-ai <khanaldavit@gmail.com>
2024-01-03 20:41:50 +0545

    dummy data added for verification of insertDummyCategoryData fucntion

--- server/controller/getRecipe.js
+++ server/controller/getRecipe.js
@@ -1,7 +1,44 @@
-require('../database/database');
+require("../database/database");
+
+const Category = require("../database/category");
 
 const getRecipe = async (req, res) => {
   res.render("index", { title: "Recipe - by Me" });
 };
+
+async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany([
+      {
+        name: "Thai",
+        image: "thai-food.jpg",
+      },
+      {
+        name: "American",
+        image: "american-food.jpg",
+      },
+      {
+        name: "Chinese",
+        image: "chinese-food.jpg",
+      },
+      {
+        name: "Mexican",
+        image: "mexican-food.jpg",
+      },
+      {
+        name: "Indian",
+        image: "indian-food.jpg",
+      },
+      {
+        name: "Spanish",
+        image: "spanish-food.jpg",
+      },
+    ]);
+  } catch (error) {
+    console.log("err ", +error);
+  }
+}
+
+insertDummyCategoryData();
 
 module.exports = getRecipe;
