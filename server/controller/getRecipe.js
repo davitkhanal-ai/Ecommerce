e63063daba6778ea9563de4f4923c19a60f7b569
davitkhanal-ai <khanaldavit@gmail.com>
2024-01-03 20:49:52 +0545

    remove unwanted code after checking the functionality of database connection

--- server/controller/getRecipe.js
+++ server/controller/getRecipe.js
@@ -6,39 +6,39 @@ const getRecipe = async (req, res) => {
   res.render("index", { title: "Recipe - by Me" });
 };
 
-async function insertDummyCategoryData() {
-  try {
-    await Category.insertMany([
-      {
-        name: "Thai",
-        image: "thai-food.jpg",
-      },
-      {
-        name: "American",
-        image: "american-food.jpg",
-      },
-      {
-        name: "Chinese",
-        image: "chinese-food.jpg",
-      },
-      {
-        name: "Mexican",
-        image: "mexican-food.jpg",
-      },
-      {
-        name: "Indian",
-        image: "indian-food.jpg",
-      },
-      {
-        name: "Spanish",
-        image: "spanish-food.jpg",
-      },
-    ]);
-  } catch (error) {
-    console.log("err ", +error);
-  }
-}
+// async function insertDummyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//       {
+//         name: "Thai",
+//         image: "thai-food.jpg",
+//       },
+//       {
+//         name: "American",
+//         image: "american-food.jpg",
+//       },
+//       {
+//         name: "Chinese",
+//         image: "chinese-food.jpg",
+//       },
+//       {
+//         name: "Mexican",
+//         image: "mexican-food.jpg",
+//       },
+//       {
+//         name: "Indian",
+//         image: "indian-food.jpg",
+//       },
+//       {
+//         name: "Spanish",
+//         image: "spanish-food.jpg",
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log("err ", +error);
+//   }
+// }
 
-insertDummyCategoryData();
+// insertDummyCategoryData();
 
 module.exports = getRecipe;
