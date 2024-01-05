22c346f00282294efdeb669154f54cc91f9f0146
davitkhanal <khanaldavit@gmail.com>
2024-01-05 21:18:20 +0545

    removed contact_me link and updated database topology

--- server/database/database.js
+++ server/database/database.js
@@ -2,13 +2,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS:5000
-  })
+  .connect(process.env.MONGO_URI, {  })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to Mongo Database on Atlas");
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
