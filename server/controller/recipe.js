53dbef0ae9039d9d7411984d29958428696b17f1
davitkhanal-ai <khanaldavit@gmail.com>
2024-01-06 19:16:55 +0545

    added recipe menu bar

--- server/controller/recipe.js
+++ server/controller/recipe.js
@@ -1,19 +1,17 @@
 require("../database/database");
 
-const Category = require("../database/category");
 const Recipe = require("../database/Recipe");
 
-const home = async (req, res) => {
+const recipe_home = async (req, res) => {
   try {
     const limitNumber = 5;
-    const categories = await Category.find({}).limit(limitNumber);
     const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
 
     const food = { latest };
-    res.render("index", { title: "Welcome to S", categories, food });
+    res.render("recipe", { title: "recipe", latest, food });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error occured" });
   }
 };
 
-module.exports = home;
+module.exports = recipe_home;
