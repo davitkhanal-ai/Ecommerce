3c9b5ecbc15cd72705823a8be468cc1637b44938
davitkhanal-ai <khanaldavit@gmail.com>
2024-01-10 19:52:02 +0545

    added thai chinese and american section

--- server/controller/home.js
+++ server/controller/home.js
@@ -8,8 +8,15 @@ const home = async (req, res) => {
     const limitNumber = 5;
     const categories = await Category.find({}).limit(limitNumber);
     const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    const thai = await Recipe.find({ category: "Thai" }).limit(limitNumber);
+    const american = await Recipe.find({ category: "American" }).limit(
+      limitNumber
+    );
+    const chinese = await Recipe.find({ category: "Chinese" }).limit(
+      limitNumber
+    );
 
-    const food = { latest };
+    const food = { latest, thai, american, chinese };
     res.render("index", { title: "Welcome to S", categories, food });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error occured" });
--- server/controller/recipe.js
+++ server/controller/recipe.js
@@ -7,7 +7,11 @@ const recipe_home = async (req, res) => {
     const limitNumber = 5;
     const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber); // ned to sort by created at here
 
-    const food = { latest };
+    const thai = await Recipe.find({ category: "Thai" }).limit(limitNumber);
+    const american = await Recipe.find({ category: "American" }).limit(limitNumber);
+    const chinese = await Recipe.find({ category: "Chinese" }).limit(limitNumber);
+
+    const food = { latest,thai,american,chinese };
     res.render("recipe", { title: "recipe", latest, food });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error occured" });
