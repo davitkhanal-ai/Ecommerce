909b51900c302291803eda74851d7a9023e362e9
davitkhanal <khanaldavit@gmail.com>
2024-01-15 20:17:07 +0545

    revert all footer pages

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -4,11 +4,13 @@ const home = require("../controller/home");
 const categories_home = require("../controller/categories_home");
 const aboutMe = require("../controller/aboutme");
 const versionTXT = require("../controller/version");
+const recipe_home = require("../controller/recipe");
+
 
 //all routes for get
 recipeRouter.get("", home);
 recipeRouter.get("/categories", categories_home);
-recipeRouter.get("/recipe", recipe);
+recipeRouter.get("/recipe", recipe_home);
 recipeRouter.get("/about", aboutMe);
 recipeRouter.get("/version.txt", versionTXT);
 
