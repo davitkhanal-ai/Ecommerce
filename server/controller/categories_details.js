bbbd9e4bf54d8d0ca3fbaafa83db5a6d50b9c01e
davit@aspire3 <khanaldavit@gmail.com>
2024-01-31 20:58:12 +0545

    remove space

--- server/controller/categories_details.js
+++ server/controller/categories_details.js
@@ -1,13 +1,14 @@
 require("../database/database");
 
-
 const Recipe = require("../database/Recipe");
 
 const categories_details = async (req, res) => {
   try {
     const limitNumber = 5;
     const categorybyId = req.params.id;
-    const categoriesbyID = await Recipe.find({'category': categorybyId }).limit(limitNumber);
+    const categoriesbyID = await Recipe.find({ category: categorybyId }).limit(
+      limitNumber
+    );
 
     res.render("categories", { title: "Categories", categoriesbyID });
   } catch (error) {
